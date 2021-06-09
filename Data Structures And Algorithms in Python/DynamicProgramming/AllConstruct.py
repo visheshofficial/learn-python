1e012c1a730461cdd630878616cf8f5aa13aa64c
RightWrite <21136428+RightWrite@users.noreply.github.com>
2021-06-09 06:20:37 +0530

    All Construct DP

--- Data Structures And Algorithms in Python/DynamicProgramming/AllConstruct.py
+++ Data Structures And Algorithms in Python/DynamicProgramming/AllConstruct.py
@@ -19,30 +19,26 @@ def allConstruct(target, word_bank):
     result = []
 
     for word in word_bank:
-        # s1=[]
         if target.startswith(word):
             suffix = target.replace(word, "")
             suffix_ways = allConstruct(suffix, word_bank)
             target_ways = []
             for x in suffix_ways:
-                a = [suffix]
-
-            result.append(target_ways)
-
-            # if result == [] or result is not None:
-            #     s1.append(word)
-        # if s1:
-        #     sub_array.append(s1)
+                a = [word]
+                a.extend(x)
+                target_ways.append(a)
+            if target_ways:
+                result.extend(target_ways)
 
     return result
 
-
+import pprint
 if __name__ == '__main__':
-    # print(countConstruct("purple", ["purp", "p", "ur", "le", "purpl"]))  # 2
+    print(allConstruct("purple", ["purp", "p", "ur", "le", "purpl"]))  # 2
     # print(countConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd"]))  # 1
-    # print(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))  # 4
-    print(allConstruct("abcdefg", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))  # 4
-    # print(countConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))  # 0
-    # print(countConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))  # 4
-    # print(countConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",
-    #                      ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]))  # 0
+    pprint.pprint(allConstruct("abcdef", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))  # 4
+    # print(allConstruct("abcdefg", ["ab", "abc", "cd", "def", "abcd", "ef", "c"]))  # 4
+    # print(allConstruct("skateboard", ["bo", "rd", "ate", "t", "ska", "sk", "boar"]))  # 0
+    print(allConstruct("enterapotentpot", ["a", "p", "ent", "enter", "ot", "o", "t"]))  # 1
+    print(allConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+                         ["e", "ee", "eee", "eeee", "eeeee", "eeeeee"]))  # 0
