3cdb4158ead51c9c50c193543a1f733b5ecc9452
RightWrite <21136428+RightWrite@users.noreply.github.com>
2021-06-09 06:21:30 +0530

    Three Largest Numbers

--- Data Structures And Algorithms in Python/Searching/ThreeLargestNumbers.py
+++ Data Structures And Algorithms in Python/Searching/ThreeLargestNumbers.py
@@ -3,15 +3,23 @@ def findThreeLargestNumbers(array):
     result.sort()
     for index in range(3, len(array)):
         if array[index] > result[2]:
-            shift_values(array, array[index], 2)
+            shift_values(result, array[index], 2)
         elif array[index] > result[1]:
-            shift_values(array, array[index], 1)
+            shift_values(result, array[index], 1)
         elif array[index] > result[0]:
-            shift_values(array, array[index], 0)
+            shift_values(result, array[index], 0)
     return result
 
 
 def shift_values(array, num, index):
-    for i in range(index + 1):
+    for i in range(index):
         array[i] = array[i + 1]
     array[index] = num
+
+
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(findThreeLargestNumbers([141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7]), [18, 141, 541])
