00b8abe43fd2bcbe54384e4c28274100ffeba861
RightWrite <21136428+RightWrite@users.noreply.github.com>
2021-06-10 23:54:24 +0530

    Depth First Search

---
+++ Data Structures And Algorithms in Python/Graph/DepthFirstSearch.py
@@ -0,0 +1,31 @@
+
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def depthFirstSearch(self, array):
+        # Write your code here.
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
+
+
+
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        graph = Node("A")
+        graph.addChild("B").addChild("C").addChild("D")
+        graph.children[0].addChild("E").addChild("F")
+        graph.children[2].addChild("G").addChild("H")
+        graph.children[0].children[1].addChild("I").addChild("J")
+        graph.children[2].children[0].addChild("K")
+        self.assertEqual(["A", "B", "E", "F", "I", "J", "C", "D", "G", "K", "H"],graph.depthFirstSearch([]))
