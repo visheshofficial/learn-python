729d5c0ec2051978b12b1c22faa8f3ea04668668
RightWrite <21136428+RightWrite@users.noreply.github.com>
2021-06-14 15:12:01 +0530

    Graph - Breadth First Search

---
+++ Data Structures And Algorithms in Python/Graph/BreadthFirstSearch.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+
+    def breadthFirstSearch(self, array):
+        # Write your code here.
+        queue = [self]
+        while len(queue) > 0:
+            cur_node = queue.pop(0)
+            for child in cur_node.children:
+                queue.append(child)
+            array.append(cur_node.name)
+        return array
+
+
+
+import unittest
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        graph = Node("A")
+        graph.addChild("B").addChild("C").addChild("D")
+        graph.children[0].addChild("E").addChild("F")
+        graph.children[2].addChild("G").addChild("H")
+        graph.children[0].children[1].addChild("I").addChild("J")
+        graph.children[2].children[0].addChild("K")
+        self.assertEqual(graph.breadthFirstSearch([]), ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K"])
